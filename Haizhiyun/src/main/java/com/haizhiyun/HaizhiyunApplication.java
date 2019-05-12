777a883c76408fb41721382e3ce952a46f55d976
haizhiyun <419328530@qq.com>
2019-05-12 21:49:53 +0800

    测试

---
+++ Haizhiyun/src/main/java/com/haizhiyun/HaizhiyunApplication.java
@@ -0,0 +1,14 @@
+package com.haizhiyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaizhiyunApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(HaizhiyunApplication.class, args);
+	}
+
+}
